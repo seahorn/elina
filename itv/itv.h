323c4940f30392d6c75cc3f29ef5a611a3a70cb1
mine <mine@d6d8869d-df28-4c9d-b3d8-ddc8926f8a8a>
2007-06-07 14:52:57 +0000

    moved ITVFUN to dedicated itv_fun.h header that (unlike itv.h) does not include num or bound
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/apron/trunk@340 d6d8869d-df28-4c9d-b3d8-ddc8926f8a8a

--- itv/itv.h
+++ itv/itv.h
@@ -11,30 +11,7 @@
 #include "itv_config.h"
 #include "ap_coeff.h"
 
-#if defined(NUM_LONGINT)
-#define NUM_SUFFIX li
-#elif defined(NUM_LONGLONGINT)
-#define NUM_SUFFIX lli
-#elif defined(NUM_MPZ)
-#define NUM_SUFFIX mpz
-#elif defined(NUM_LONGRAT)
-#define NUM_SUFFIX lr
-#elif defined(NUM_LONGLONGRAT)
-#define NUM_SUFFIX llr
-#elif defined(NUM_MPQ)
-#define NUM_SUFFIX mpq
-#elif defined(NUM_DOUBLE)
-#define NUM_SUFFIX dbl
-#elif defined(NUM_LONGDOUBLE)
-#define NUM_SUFFIX ldbl
-#else
-#error "HERE"
-#endif
-
-/* ITVFUN(itv_add) expands to itv_add_mpq, or itv_add_dbl, ... */
-#define ITVFUN3(NAME,SUFFIX) NAME ## _ ## SUFFIX
-#define ITVFUN2(x,y) ITVFUN3(x,y)
-#define ITVFUN(x) ITVFUN2(x,NUM_SUFFIX)
+#include "itv_fun.h"
 
 #ifdef __cplusplus
 extern "C" {
