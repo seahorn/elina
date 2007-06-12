981b42fa75eae76bc8351e55311fc587020519d1
jeannet <jeannet@d6d8869d-df28-4c9d-b3d8-ddc8926f8a8a>
2007-06-12 08:35:41 +0000

    - new module ap_reduceproduct
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/apron/trunk@350 d6d8869d-df28-4c9d-b3d8-ddc8926f8a8a

---
+++ apron/ap_reducedproduct.h
@@ -0,0 +1,53 @@
+/* ************************************************************************* */
+/* reducedproduct.h: generic reduced product of N domains */
+/* ************************************************************************* */
+
+/* This file is part of the APRON Library, released under LGPL license.  Please
+   read the COPYING file packaged in the distribution */
+
+#ifndef _AP_REDUCEDPRODUCT_H_
+#define _AP_REDUCEDPRODUCT_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "ap_global0.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* (internal) abstract value for a product */
+typedef struct ap_reducedproduct_t {
+  bool reduced;
+  void* p[0];
+} ap_reducedproduct_t;
+
+/* internal field of manager */
+typedef struct ap_reducedproduct_internal_t {
+  void (*reduce)(ap_manager_t*, ap_reducedproduct_t*);             /* reduce function */
+  void (*approximate)(ap_manager_t*, ap_reducedproduct_t*, int n); /* approximate function */
+  size_t size;                                       /* size of the product */
+  ap_manager_t* tmanagers[0];                        /* of size size */
+} ap_reducedproduct_internal_t;
+
+ap_manager_t* ap_reducedproduct_manager_alloc
+(
+ ap_manager_t** tab, /* Array of managers */
+ size_t size,        /* size of array */
+ void (*reduce)(ap_manager_t*, ap_reducedproduct_t*),            
+   /* reduce function */
+ void (*approximate)(ap_manager_t*, ap_reducedproduct_t*, int n) 
+   /* approximate function */
+);
+  
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
